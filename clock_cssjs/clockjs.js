47fd69f277ce28eb66da2f2ce46862e1718ad7a2
Ario Setiawan <consciousness.dev@gmail.com>
2018-02-12 06:24:08 +0700

    Done Fix get mins and hour of real time, fixing real formula for real time & commenting file clockjs.js for informations

--- clock_cssjs/clockjs.js
+++ clock_cssjs/clockjs.js
@@ -5,17 +5,36 @@ const hourHand = document.querySelector('.hour-hand');
 function setDate() {
     const now = new Date();
 
+    // get seconds from new Date() of now
     const seconds = now.getSeconds();
+
+    // formula of convert milisecond to real second => ((seconds / 60) * 360) + 90;
     const secondsDegrees = ((seconds / 60) * 360) + 90;
+
+    // set style of seconds hand in css
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-    const mins = new Date();
-    const minsDegrees = ((mins / 60) * 360) + 90;
+    // get minutes from new Date() of now
+    const mins = now.getMinutes();
+
+    // formula of convert milisecond to real minutes => ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+    const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+
+    // set style of minutes hand in css
     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
-    const hour = new Date();
-    const hourDegrees = ((hour / 12) * 360) + 90;
+    // get hour from new Date() of now
+    const hour = now.getHours();
+
+    // formula of convert milisecond to real minutes => ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+    const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+
+    // set style of hour hand in css
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
+// setInterval function of date which is we coding it, set milisecond to 1000 for real time interval
 setInterval(setDate, 1000);
+
+// Runs it setDate functions to Clock Web Pages!!!
+setDate();
