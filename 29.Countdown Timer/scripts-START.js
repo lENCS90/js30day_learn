fae987fb42d141882f531d7a1d91dec28c6ce1d8
ario.baruna <consciousness.dev@gmail.com>
2020-03-06 09:08:06 +0700

    Part 29 done

--- 29.Countdown Timer/scripts-START.js
+++ 29.Countdown Timer/scripts-START.js
@@ -4,8 +4,16 @@ let countdown;
 // select timer display element
 const timerDisplay = document.querySelector('.display__time-left');
 
+// select end time element
+const endTime = document.querySelector('.display__end-time');
+
+// select buttons and form
+const buttons = document.querySelectorAll('[data-time]');
+
 // Function to run countdown timer
 function timer(seconds) {
+    // clear running timer
+    clearInterval(countdown);
     // get time of now
     const now = Date.now();
 
@@ -15,6 +23,9 @@ function timer(seconds) {
     // display time left on running time
     displayTimeLeft(seconds);
 
+    // display backtime
+    displayEndTime(then);
+
     // set interval of seconds time send to countdown
     countdown = setInterval(() => {
 
@@ -23,7 +34,7 @@ function timer(seconds) {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
 
         // check if got 0 seconds left
-        if (secondsLeft < 0) {
+        if (secondsLeft <= 0) {
 
             // callback of stop interval
             clearInterval(countdown);
@@ -49,3 +60,49 @@ function displayTimeLeft(seconds) {
     // set timerdisplay
     timerDisplay.textContent = display;
 }
+
+// Function to display end after break from countdown
+function displayEndTime(timestamp) {
+    // selector of timestamp end date
+    const end = new Date(timestamp);
+
+    // get timestamp hour
+    const hour = end.getHours();
+
+    // get timestamp minute
+    const minutes = end.getMinutes();
+
+    // set endtime 
+    // endTime.textContent = `Be Back at ${hour}:${minutes}`;
+
+    // convert to AM/PM
+    endTime.textContent = `Be Back at ${hour > 12 ? hour - 12 : hour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
+
+// function start timer
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    // call to timer function
+    timer(seconds);
+}
+
+// set button event binding with start timer function
+buttons.forEach(button =>
+    button.addEventListener('click', startTimer)
+);
+
+// set sumbit form event
+document.customForm.addEventListener('submit', function (e) {
+    // stop submit reload
+    e.preventDefault();
+
+    // select minutes form value
+    const mins = this.minutes.value;
+
+    // set timer with mins
+    timer(mins * 60);
+
+    // reset form
+    this.reset();
+
+})
